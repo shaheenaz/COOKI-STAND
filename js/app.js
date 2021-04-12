b5b57a3eadd4d3a01bb6c0648ecb08505e0d052b
shaheenaz <shaheenaz99@gmail.com>
2021-04-12 14:07:23 +0300

    adding features

--- js/app.js
+++ js/app.js
@@ -1,206 +1,229 @@
-'use strict'
-const seatle = {
-max:65,
-min:23,
-avarge:6.3,
-total:0,
-hours:['6am:','7am:','8am:','9am:','10am:','11am:','12pm:','1pm:','2pm:','3pm:','4pm:','5pm:','6pm:','7pm:'],
-cookeisperHour:[],
-calcCookies:function(){
-for(let i=0; i<14 ; i++){
-this.cookeisperHour.push(Math.ceil(getRandomIntInclusive(23,65)*this.avarge))
-
-}},
-createList:function(){
-let container = document.getElementById('cookie');
-let h2 = document.createElement('h2')
-container.appendChild(h2);
-h2.textContent ="seatle"
-let lists = document.createElement('ul')
-container.appendChild(lists)
-for(let i=0; i<this.hours.length ;i++){
-    let li = document.createElement('li')
-    lists.appendChild(li)
-    // console.log(this.cookeisperHour)
-    li.textContent = this.hours [i] + this.cookeisperHour[i] 
-    //console.log(this.hours.length)
-    
-}let li = document.createElement('li')
-this.total = this.cookeisperHour.reduce((a, b) => a + b, 0)
-lists.appendChild(li) 
-li.textContent =`total ${this.total}`
-console.log(this.cookeisperHour.reduce((a, b) => a + b, 0))
-
-},
+ 'use strict'
+ function getRandomIntInclusive(min, max){
+        return Math.floor(Math.random() * (max - min + 1) + min)}
+       let array = []
+function Cookies(cityName,max,min,avarage){
+    this.name= cityName;
+    this.max =max ;
+    this.min = min ;
+    this.avarage = avarage;
+    this.cookiesperHour = [];
+    this.total = 0;
+    this. hours = ['6am:','7am:','8am:','9am:','10am:','11am:','12pm:','1pm:','2pm:','3pm:','4pm:','5pm:','6pm:','7pm:'];
+array.push(this)
 
 }
-seatle.calcCookies()
-seatle.createList()
 
-function getRandomIntInclusive(min, max){
-    return Math.floor(Math.random() * (max - min + 1) + min)}
+let city1= new Cookies ('seatle',23,65,6.3)
+console.log(this)
+
+
+
+
+
+
+// const seatle = {
+// max:65,
+// min:23,
+// avarge:6.3,
+// total:0,
+// hours:['6am:','7am:','8am:','9am:','10am:','11am:','12pm:','1pm:','2pm:','3pm:','4pm:','5pm:','6pm:','7pm:'],
+// cookeisperHour:[],
+// calcCookies:function(){
+// for(let i=0; i<14 ; i++){
+// this.cookeisperHour.push(Math.ceil(getRandomIntInclusive(23,65)*this.avarge))
+
+// }},
+// createList:function(){
+// let container = document.getElementById('cookie');
+// let h2 = document.createElement('h2')
+// container.appendChild(h2);
+// h2.textContent ="seatle"
+// let lists = document.createElement('ul')
+// container.appendChild(lists)
+// for(let i=0; i<this.hours.length ;i++){
+//     let li = document.createElement('li')
+//     lists.appendChild(li)
+//     // console.log(this.cookeisperHour)
+//     li.textContent = this.hours [i] + this.cookeisperHour[i] 
+//     //console.log(this.hours.length)
+    
+// }let li = document.createElement('li')
+// this.total = this.cookeisperHour.reduce((a, b) => a + b, 0)
+// lists.appendChild(li) 
+// li.textContent =`total ${this.total}`
+// console.log(this.cookeisperHour.reduce((a, b) => a + b, 0))
+
+// },
+
+// }
+// seatle.calcCookies()
+// seatle.createList()
+
+// function getRandomIntInclusive(min, max){
+//     return Math.floor(Math.random() * (max - min + 1) + min)}
 
     
-const tokyo = {
-max:24,
-min:3,
-avarge:1.2,
-total:0,
-hours:['6am:','7am:','8am:','9am:','10am:','11am:','12pm:','1pm:','2pm:','3pm:','4pm:','5pm:','6pm:','7pm:'],
-cookeisperHour:[],
-calcCookies:function(){
-for(let i=0; i<14 ; i++){
-this.cookeisperHour.push(Math.ceil(getRandomIntInclusive(23,65)*this.avarge))
+// const tokyo = {
+// max:24,
+// min:3,
+// avarge:1.2,
+// total:0,
+// hours:['6am:','7am:','8am:','9am:','10am:','11am:','12pm:','1pm:','2pm:','3pm:','4pm:','5pm:','6pm:','7pm:'],
+// cookeisperHour:[],
+// calcCookies:function(){
+// for(let i=0; i<14 ; i++){
+// this.cookeisperHour.push(Math.ceil(getRandomIntInclusive(23,65)*this.avarge))
 
-}},
-createList:function(){
-let container = document.getElementById('cookie');
-let h2 = document.createElement('h2')
-container.appendChild(h2);
-h2.textContent ="tokyo"
-let lists = document.createElement('ul')
-container.appendChild(lists)
-for(let i=0; i<this.hours.length ;i++){
-    let li = document.createElement('li')
-    lists.appendChild(li)
-    // console.log(this.cookeisperHour)
-    li.textContent = this.hours [i] + this.cookeisperHour[i] 
-    //console.log(this.hours.length)
+// }},
+// createList:function(){
+// let container = document.getElementById('cookie');
+// let h2 = document.createElement('h2')
+// container.appendChild(h2);
+// h2.textContent ="tokyo"
+// let lists = document.createElement('ul')
+// container.appendChild(lists)
+// for(let i=0; i<this.hours.length ;i++){
+//     let li = document.createElement('li')
+//     lists.appendChild(li)
+//     // console.log(this.cookeisperHour)
+//     li.textContent = this.hours [i] + this.cookeisperHour[i] 
+//     //console.log(this.hours.length)
     
-}let li = document.createElement('li')
-this.total = this.cookeisperHour.reduce((a, b) => a + b, 0)
-lists.appendChild(li) 
-li.textContent =`total ${this.total}`
-console.log(this.cookeisperHour.reduce((a, b) => a + b, 0))
+// }let li = document.createElement('li')
+// this.total = this.cookeisperHour.reduce((a, b) => a + b, 0)
+// lists.appendChild(li) 
+// li.textContent =`total ${this.total}`
+// console.log(this.cookeisperHour.reduce((a, b) => a + b, 0))
 
-},
+// },
 
-}
-tokyo.calcCookies()
-tokyo.createList()
+// }
+// tokyo.calcCookies()
+// tokyo.createList()
 
-function getRandomIntInclusive(min, max){
-    return Math.floor(Math.random() * (max - min + 1) + min)}
+// function getRandomIntInclusive(min, max){
+//     return Math.floor(Math.random() * (max - min + 1) + min)}
     
-const dubai = {
-max:38,
-min:11,
-avarge:3.7,
-total:0,
-hours:['6am:','7am:','8am:','9am:','10am:','11am:','12pm:','1pm:','2pm:','3pm:','4pm:','5pm:','6pm:','7pm:'],
-cookeisperHour:[],
-calcCookies:function(){
-for(let i=0; i<14 ; i++){
-this.cookeisperHour.push(Math.ceil(getRandomIntInclusive(23,65)*this.avarge))
+// const dubai = {
+// max:38,
+// min:11,
+// avarge:3.7,
+// total:0,
+// hours:['6am:','7am:','8am:','9am:','10am:','11am:','12pm:','1pm:','2pm:','3pm:','4pm:','5pm:','6pm:','7pm:'],
+// cookeisperHour:[],
+// calcCookies:function(){
+// for(let i=0; i<14 ; i++){
+// this.cookeisperHour.push(Math.ceil(getRandomIntInclusive(23,65)*this.avarge))
 
-}},
-createList:function(){
-let container = document.getElementById('cookie');
-let h2 = document.createElement('h2')
-container.appendChild(h2);
-h2.textContent ="Dubai"
-let lists = document.createElement('ul')
-container.appendChild(lists)
-for(let i=0; i<this.hours.length ;i++){
-    let li = document.createElement('li')
-    lists.appendChild(li)
-    // console.log(this.cookeisperHour)
-    li.textContent = this.hours [i] + this.cookeisperHour[i] 
-    //console.log(this.hours.length)
+// }},
+// createList:function(){
+// let container = document.getElementById('cookie');
+// let h2 = document.createElement('h2')
+// container.appendChild(h2);
+// h2.textContent ="Dubai"
+// let lists = document.createElement('ul')
+// container.appendChild(lists)
+// for(let i=0; i<this.hours.length ;i++){
+//     let li = document.createElement('li')
+//     lists.appendChild(li)
+//     // console.log(this.cookeisperHour)
+//     li.textContent = this.hours [i] + this.cookeisperHour[i] 
+//     //console.log(this.hours.length)
     
-}let li = document.createElement('li')
-this.total = this.cookeisperHour.reduce((a, b) => a + b, 0)
-lists.appendChild(li) 
-li.textContent =`total ${this.total}`
-console.log(this.cookeisperHour.reduce((a, b) => a + b, 0))
+// }let li = document.createElement('li')
+// this.total = this.cookeisperHour.reduce((a, b) => a + b, 0)
+// lists.appendChild(li) 
+// li.textContent =`total ${this.total}`
+// console.log(this.cookeisperHour.reduce((a, b) => a + b, 0))
 
-},
+// },
 
-}
-dubai.calcCookies()
-dubai.createList()
+// }
+// dubai.calcCookies()
+// dubai.createList()
 
-function getRandomIntInclusive(min, max){
-    return Math.floor(Math.random() * (max - min + 1) + min)}
+// function getRandomIntInclusive(min, max){
+//     return Math.floor(Math.random() * (max - min + 1) + min)}
     
-const paris = {
-max:38,
-min:20,
-avarge:2.3,
-total:0,
-hours:['6am:','7am:','8am:','9am:','10am:','11am:','12pm:','1pm:','2pm:','3pm:','4pm:','5pm:','6pm:','7pm:'],
-cookeisperHour:[],
-calcCookies:function(){
-for(let i=0; i<14 ; i++){
-this.cookeisperHour.push(Math.ceil(getRandomIntInclusive(23,65)*this.avarge))
+// const paris = {
+// max:38,
+// min:20,
+// avarge:2.3,
+// total:0,
+// hours:['6am:','7am:','8am:','9am:','10am:','11am:','12pm:','1pm:','2pm:','3pm:','4pm:','5pm:','6pm:','7pm:'],
+// cookeisperHour:[],
+// calcCookies:function(){
+// for(let i=0; i<14 ; i++){
+// this.cookeisperHour.push(Math.ceil(getRandomIntInclusive(23,65)*this.avarge))
 
-}},
-createList:function(){
-let container = document.getElementById('cookie');
-let h2 = document.createElement('h2')
-container.appendChild(h2);
-h2.textContent ="paris"
-let lists = document.createElement('ul')
-container.appendChild(lists)
-for(let i=0; i<this.hours.length ;i++){
-    let li = document.createElement('li')
-    lists.appendChild(li)
-    // console.log(this.cookeisperHour)
-    li.textContent = this.hours [i] + this.cookeisperHour[i] 
-    //console.log(this.hours.length)
+// }},
+// createList:function(){
+// let container = document.getElementById('cookie');
+// let h2 = document.createElement('h2')
+// container.appendChild(h2);
+// h2.textContent ="paris"
+// let lists = document.createElement('ul')
+// container.appendChild(lists)
+// for(let i=0; i<this.hours.length ;i++){
+//     let li = document.createElement('li')
+//     lists.appendChild(li)
+//     // console.log(this.cookeisperHour)
+//     li.textContent = this.hours [i] + this.cookeisperHour[i] 
+//     //console.log(this.hours.length)
     
-}let li = document.createElement('li')
-this.total = this.cookeisperHour.reduce((a, b) => a + b, 0)
-lists.appendChild(li) 
-li.textContent =`total ${this.total}`
-console.log(this.cookeisperHour.reduce((a, b) => a + b, 0))
+// }let li = document.createElement('li')
+// this.total = this.cookeisperHour.reduce((a, b) => a + b, 0)
+// lists.appendChild(li) 
+// li.textContent =`total ${this.total}`
+// console.log(this.cookeisperHour.reduce((a, b) => a + b, 0))
 
-},
+// },
 
-}
-paris.calcCookies()
-paris.createList()
+// }
+// paris.calcCookies()
+// paris.createList()
 
-function getRandomIntInclusive(min, max){
-    return Math.floor(Math.random() * (max - min + 1) + min)}
+// function getRandomIntInclusive(min, max){
+//     return Math.floor(Math.random() * (max - min + 1) + min)}
     
-const lima = {
-max:16,
-min:2,
-avarge:4.6,
-total:0,
-hours:['6am:','7am:','8am:','9am:','10am:','11am:','12pm:','1pm:','2pm:','3pm:','4pm:','5pm:','6pm:','7pm:'],
-cookeisperHour:[],
-calcCookies:function(){
-for(let i=0; i<14 ; i++){
-this.cookeisperHour.push(Math.ceil(getRandomIntInclusive(23,65)*this.avarge))
+// const lima = {
+// max:16,
+// min:2,
+// avarge:4.6,
+// total:0,
+// hours:['6am:','7am:','8am:','9am:','10am:','11am:','12pm:','1pm:','2pm:','3pm:','4pm:','5pm:','6pm:','7pm:'],
+// cookeisperHour:[],
+// calcCookies:function(){
+// for(let i=0; i<14 ; i++){
+// this.cookeisperHour.push(Math.ceil(getRandomIntInclusive(23,65)*this.avarge))
 
-}},
-createList:function(){
-let container = document.getElementById('cookie');
-let h2 = document.createElement('h2')
-container.appendChild(h2);
-h2.textContent ="lima"
-let lists = document.createElement('ul')
-container.appendChild(lists)
-for(let i=0; i<this.hours.length ;i++){
-    let li = document.createElement('li')
-    lists.appendChild(li)
-    // console.log(this.cookeisperHour)
-    li.textContent = this.hours [i] + this.cookeisperHour[i] 
-    //console.log(this.hours.length)
+// }},
+// createList:function(){
+// let container = document.getElementById('cookie');
+// let h2 = document.createElement('h2')
+// container.appendChild(h2);
+// h2.textContent ="lima"
+// let lists = document.createElement('ul')
+// container.appendChild(lists)
+// for(let i=0; i<this.hours.length ;i++){
+//     let li = document.createElement('li')
+//     lists.appendChild(li)
+//     // console.log(this.cookeisperHour)
+//     li.textContent = this.hours [i] + this.cookeisperHour[i] 
+//     //console.log(this.hours.length)
     
-}let li = document.createElement('li')
-this.total = this.cookeisperHour.reduce((a, b) => a + b, 0)
-lists.appendChild(li) 
-li.textContent =`total ${this.total}`
-console.log(this.cookeisperHour.reduce((a, b) => a + b, 0))
+// }let li = document.createElement('li')
+// this.total = this.cookeisperHour.reduce((a, b) => a + b, 0)
+// lists.appendChild(li) 
+// li.textContent =`total ${this.total}`
+// console.log(this.cookeisperHour.reduce((a, b) => a + b, 0))
 
-},
+// },
 
-}
-lima.calcCookies()
-lima.createList()
+// }
+// lima.calcCookies()
+// lima.createList()
 
-function getRandomIntInclusive(min, max){
-    return Math.floor(Math.random() * (max - min + 1) + min)}
+// function getRandomIntInclusive(min, max){
+//     return Math.floor(Math.random() * (max - min + 1) + min)}
